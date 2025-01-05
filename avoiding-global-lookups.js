a2c2abe4b5290ba0dbe5106659fcb7beb6bae5c3
Chen Hom <orangecatchen@gmail.com>
2025-01-05 15:36:24 +0800

    feat: 增加 DOM 操作性能測試與優化建議
    在頁面加載完成後，建立 2000 個盒子並添加點擊事件，測試使用全域物件 `document` 的性能影響。更新 README.md 文件，詳細說明性能測試的過程與結果，並提供優化建議。

--- avoiding-global-lookups.js
+++ avoiding-global-lookups.js
@@ -1,10 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
     const container = document.getElementById('container');
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < 2000; i++) {
         const box = document.createElement('div');
         box.textContent = `Box #${i + 1}`;
         box.style.margin = '8px';
         box.style.backgroundColor = '#ccc';
+        box.style.display = 'inline-block';
         container.appendChild(box);
     }
 });
+
+document.addEventListener('click', () => {
+    // const boxes = container.children;
+    // const doc = document;
+    // console.time('querySelectorAll2');
+    console.time('querySelectorAll');
+    // const boxes = doc.querySelectorAll('div');
+    const boxes = document.querySelectorAll('div');
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].style.backgroundColor = 'lightblue';
+    }
+    // console.timeEnd('querySelectorAll2');
+    console.timeEnd('querySelectorAll');
+});
+
